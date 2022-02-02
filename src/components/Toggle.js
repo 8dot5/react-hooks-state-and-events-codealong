e6144e93eeea6aeebcc6f0843ddd1d446465456f
Chun Yi <8.5by11@gmail.com>
2022-02-02 18:58:40 -0500

    Done

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+
+/*Deliverables
+The button should say "OFF" when it is first displayed
+When the button is clicked, it should say "ON"
+When the button is clicked again, it should say "OFF"
+etc
+*/
 
 function Toggle() {
-  return <button>OFF</button>;
+  let [ isOn, setIsOn ] = useState(false)
+
+  function handleClick() {
+    setIsOn((isOn) => !isOn)
+  }
+  return (
+    <button onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>
+  );
 }
 
 export default Toggle;
